8dc5bdc3fd9efdfdc7d826671af0e2cf0d786344
Caleb Gardner <calebq23@gmail.com>
2021-01-20 00:00:01 -0600

    Thumbnail work is now fully in memory.

--- go.mod
+++ go.mod
@@ -6,24 +6,24 @@ require (
 	github.com/CalebQ42/squashfs v0.3.12
 	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
 	github.com/acobaugh/osrelease v0.0.0-20181218015638-a93a0a55a249
-	github.com/adrg/xdg v0.2.3
+	github.com/adrg/xdg v0.3.0
 	github.com/alokmenghrajani/gpgeez v0.0.0-20161206084504-1a06f1c582f9
 	github.com/coreos/go-systemd/v22 v22.1.0
-	github.com/eclipse/paho.mqtt.golang v1.3.0
+	github.com/eclipse/paho.mqtt.golang v1.3.1
 	github.com/esiqveland/notify v0.9.1
-	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
 	github.com/godbus/dbus/v5 v5.0.3
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/grandcat/zeroconf v1.0.0
 	github.com/h2non/go-is-svg v0.0.0-20160927212452-35e8c4b0612c
-	github.com/hashicorp/go-version v1.2.0
-	github.com/otiai10/copy v1.4.1
+	github.com/hashicorp/go-version v1.2.1
+	github.com/otiai10/copy v1.4.2
 	github.com/probonopd/go-zsyncmake v0.0.0-20181008012426-5db478ac2be7
-	github.com/prometheus/procfs v0.2.0
+	github.com/prometheus/procfs v0.3.0
 	github.com/rjeczalik/notify v0.9.2
 	github.com/sabhiram/png-embed v0.0.0-20180421025336-149afe9a3ccb
 	github.com/sabhiram/pngr v0.0.0-20180419043407-2df49b015d4b // indirect
-	github.com/shirou/gopsutil v3.20.11+incompatible
+	github.com/shirou/gopsutil v3.20.12+incompatible
 	github.com/shuheiktgw/go-travis v0.3.1
 	github.com/srwiley/oksvg v0.0.0-20200311192757-870daf9aa564
 	github.com/srwiley/rasterx v0.0.0-20200120212402-85cb7272f5e9
@@ -31,7 +31,7 @@ require (
 	go.lsp.dev/uri v0.3.0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	golang.org/x/image v0.0.0-20201208152932-35266b937fa6 // indirect
-	golang.org/x/sys v0.0.0-20201221093633-bc327ba9c2f0
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4
 	gopkg.in/ini.v1 v1.62.0
 	gopkg.in/src-d/go-git.v4 v4.13.1
 )
--- src/appimaged/thumbnail.go
+++ src/appimaged/thumbnail.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"fmt"
 	"image"
 	"image/png"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"time"
@@ -14,8 +14,8 @@ import (
 	issvg "github.com/h2non/go-is-svg"
 	"github.com/probonopd/go-appimage/internal/helpers"
 	pngembed "github.com/sabhiram/png-embed" // For embedding metadata into PNG
-	. "github.com/srwiley/oksvg"             // https://github.com/niemeyer/gopkg/issues/72
-	. "github.com/srwiley/rasterx"
+	"github.com/srwiley/oksvg"
+	"github.com/srwiley/rasterx"
 )
 
 /* The thumbnail cache directory is prefixed with $XDG_CACHE_DIR/ and the leading dot removed
@@ -32,23 +32,9 @@ func (ai AppImage) extractDirIconAsThumbnail() {
 	// TODO: Detect Modifications by reading the 'Thumb::MTime' key as per
 	// https://specifications.freedesktop.org/thumbnail-spec/thumbnail-spec-latest.html#MODIFICATIONS
 
-	// Write out the icon to a temporary location
-	thumbnailcachedir := xdg.CacheHome + "/thumbnails/" + ai.md5
-	os.MkdirAll(thumbnailcachedir, os.ModePerm)
-
-	// if ai.imagetype == 1 {
-	// 	err := os.MkdirAll(thumbnailcachedir, os.ModePerm)
-	// 	helpers.LogError("thumbnail: thumbnailcachedir", err)
-	// 	cmd := exec.Command("bsdtar", "-C", thumbnailcachedir, "-xf", ai.path, ".DirIcon")
-	// 	runCommand(cmd)
-	// } else if ai.imagetype == 2 {
-	// 	// TODO: first list contents of the squashfs, then determine what to extract
-	// 	cmd := exec.Command("unsquashfs", "-f", "-n", "-o", strconv.FormatInt(ai.offset, 10), "-d", thumbnailcachedir, ai.path, ".DirIcon")
-	// 	runCommand(cmd)
-	// }
-
 	//this will try to extract the thumbnail, or goes back to command based extraction if it fails.
-	var dirIconFil *os.File
+	var buf *bytes.Buffer
+	var data []byte
 	dirIconRdr, err := ai.Thumbnail()
 	if err != nil {
 		if *verbosePtr {
@@ -59,10 +45,8 @@ func (ai AppImage) extractDirIconAsThumbnail() {
 			goto genericIcon
 		}
 	}
-	dirIconFil, _ = os.Create(thumbnailcachedir + "/.DirIcon")
-	_, err = io.Copy(dirIconFil, dirIconRdr)
+	_, err = io.Copy(buf, dirIconRdr)
 	dirIconRdr.Close()
-	dirIconFil.Close()
 	if err != nil {
 		helpers.LogError("thumbnail", err)
 	}
@@ -72,45 +56,26 @@ func (ai AppImage) extractDirIconAsThumbnail() {
 	// sendErrorDesktopNotification(ai.niceName+" may be defective", "Could not read .DirIcon")
 	// }
 genericIcon:
-	buf, err := ioutil.ReadFile(thumbnailcachedir + "/.DirIcon")
-	if os.IsNotExist(err) {
+	if buf.Len() == 0 {
 		if *verbosePtr == true {
-			log.Printf("Could not extract icon, use default icon instead: %s\n", thumbnailcachedir+"/.DirIcon")
+			log.Println("Could not extract icon, use default icon instead")
 		}
-		data, err := Asset("data/appimage.png")
+		data, err = Asset("data/appimage.png")
 		helpers.LogError("thumbnail", err)
-		err = os.MkdirAll(thumbnailcachedir, 0755)
-		helpers.LogError("thumbnail", err)
-		err = ioutil.WriteFile(thumbnailcachedir+"/.DirIcon", data, 0644)
-		helpers.LogError("thumbnail", err)
-	} else if err != nil {
-		log.Printf("Error: %s\n", err)
+		buf = bytes.NewBuffer(data)
 	}
-	if issvg.Is(buf) {
+	if issvg.Is(buf.Bytes()) {
 		log.Println("thumbnail: .DirIcon in", ai.Path, "is an SVG, this is discouraged. Costly converting it now")
-		err = convertToPng(thumbnailcachedir + "/.DirIcon")
+		buf, err = convertToPng(buf)
 		helpers.LogError("thumbnail", err)
 	}
 
 	// Before we proceed, delete empty files. Otherwise the following operations can crash
 	// TODO: Better check if it is a PNG indeed
-	fi, err := os.Stat(thumbnailcachedir + "/.DirIcon")
-	helpers.LogError("thumbnail", err)
-	if err != nil {
-		return
-	} else if fi.Size() == 0 {
-		err = os.Remove(thumbnailcachedir + "/.DirIcon")
-		helpers.LogError("thumbnail", err)
-		return
-	}
 
-	f, err := os.Open(thumbnailcachedir + "/.DirIcon")
-	defer f.Close()
-
-	if helpers.CheckMagicAtOffset(f, "504e47", 1) == false {
-		log.Println("thumbnail: Not a PNG file, hence removing:", thumbnailcachedir+"/.DirIcon")
-		err = os.Remove(thumbnailcachedir + "/.DirIcon")
-		helpers.LogError("thumbnail", err)
+	if buf.Len() == 0 {
+		helpers.LogError("thumbnail", fmt.Errorf("No thumbnail"))
+		return
 	}
 
 	// Write "Thumbnail Attributes" metadata as mandated by
@@ -122,36 +87,37 @@ genericIcon:
 	// FIXME; github.com/sabhiram/png-embed does not overwrite pre-existing values,
 	// https://github.com/sabhiram/png-embed/issues/1
 
-	content, err := pngembed.ExtractFile(thumbnailcachedir + "/.DirIcon")
-
-	if *verbosePtr == true {
-		if _, ok := content["Thumb::URI"]; ok {
-			log.Println("thumbnail: FIXME: Remove pre-existing Thumb::URI in", ai.Path)
-			// log.Println(content["Thumb::URI"])
-		}
-		if _, ok := content["Thumb::MTime"]; ok {
-			log.Println("thumbnail: FIXME: Remove pre-existing Thumb::MTime", content["Thumb::MTime"], "in", ai.Path) // FIXME; pngembed does not seem to overwrite pre-existing values, is it a bug there?
-			// log.Println(content["Thumb::MTime"])
-		}
-	}
-	helpers.LogError("thumbnail "+thumbnailcachedir+"/.DirIcon", err)
-	data, err := pngembed.EmbedFile(thumbnailcachedir+"/.DirIcon", "Thumb::URI", ai.uri)
-	helpers.LogError("thumbnail", err)
-
-	/* Set 'Thumb::MTime' metadata of the thumbnail file to the mtime of the AppImage.
-	NOTE: https://specifications.freedesktop.org/thumbnail-spec/thumbnail-spec-latest.html#MODIFICATIONS says:
-	It is not sufficient to do a file.mtime > thumb.MTime check.
-	If the user moves another file over the original, where the mtime changes but is in fact lower
-	than the thumbnail stored mtime, we won't recognize this modification.
-	If for some reason the thumbnail doesn't have the 'Thumb::MTime' key (although it's required)
-	it should be recreated in any case. */
-	if appImageInfo, err := os.Stat(ai.Path); err == nil {
-		_, err := pngembed.EmbedFile(thumbnailcachedir+"/.DirIcon", "Thumb::MTime", appImageInfo.ModTime())
-		helpers.LogError("thumbnail", err)
-	}
-
+	content, err := pngembed.Extract(buf.Bytes())
 	if err == nil {
-		err = ioutil.WriteFile(thumbnailcachedir+"/.DirIcon", data, 600)
+		if *verbosePtr == true {
+			if _, ok := content["Thumb::URI"]; ok {
+				log.Println("thumbnail: FIXME: Remove pre-existing Thumb::URI in", ai.Path)
+				// log.Println(content["Thumb::URI"])
+			}
+			if _, ok := content["Thumb::MTime"]; ok {
+				log.Println("thumbnail: FIXME: Remove pre-existing Thumb::MTime", content["Thumb::MTime"], "in", ai.Path) // FIXME; pngembed does not seem to overwrite pre-existing values, is it a bug there?
+				// log.Println(content["Thumb::MTime"])
+			}
+		}
+		helpers.LogError("thumbnail", err)
+		data, err = pngembed.Embed(buf.Bytes(), "Thumb::URI", ai.uri)
+		if err != nil {
+			helpers.LogError("thumbnail", err)
+			buf = bytes.NewBuffer(data)
+		}
+		/* Set 'Thumb::MTime' metadata of the thumbnail file to the mtime of the AppImage.
+		NOTE: https://specifications.freedesktop.org/thumbnail-spec/thumbnail-spec-latest.html#MODIFICATIONS says:
+		It is not sufficient to do a file.mtime > thumb.MTime check.
+		If the user moves another file over the original, where the mtime changes but is in fact lower
+		than the thumbnail stored mtime, we won't recognize this modification.
+		If for some reason the thumbnail doesn't have the 'Thumb::MTime' key (although it's required)
+		it should be recreated in any case. */
+		data, err = pngembed.Embed(buf.Bytes(), "Thumb::MTime", ai.ModTime())
+		if err != nil {
+			helpers.LogError("thumbnail", err)
+			buf = bytes.NewBuffer(data)
+		}
+	} else {
 		helpers.LogError("thumbnail", err)
 	}
 
@@ -166,21 +132,33 @@ genericIcon:
 	helpers.LogError("thumbnail", err)
 
 	if *verbosePtr == true {
-		log.Println("thumbnail: Moving", thumbnailcachedir+"/.DirIcon", "to", ai.thumbnailfilepath)
+		log.Println("thumbnail: Creating", ai.thumbnailfilepath)
 	}
 
-	err = os.Rename(thumbnailcachedir+"/.DirIcon", ai.thumbnailfilepath)
 	helpers.LogError("thumbnail", err)
+
+	thumbnail, err := os.Create(ai.thumbnailfilepath)
+	if os.IsExist(err) {
+		if *verbosePtr == true {
+			log.Println("thumbnail:", ai.thumbnailfilepath, "exists. Deleting and writing.")
+		}
+		os.Remove(ai.thumbnailfilename)
+		thumbnail, err = os.Create(ai.thumbnailfilepath)
+		if err != nil {
+			helpers.LogError("thumbnail", err)
+			return
+		}
+	}
+	_, err = io.Copy(thumbnail, buf)
+	if err != nil {
+		helpers.LogError("thumbnail", err)
+		return
+	}
 
 	/* Also set mtime of the thumbnail file to the mtime of the AppImage. Quite possibly this is not needed.
 	TODO: Perhaps we can remove it.
 	See https://specifications.freedesktop.org/thumbnail-spec/thumbnail-spec-latest.html#MODIFICATIONS  */
-	if appImageInfo, err := os.Stat(ai.Path); err == nil {
-		err := os.Chtimes(ai.thumbnailfilepath, time.Now().Local(), appImageInfo.ModTime())
-		helpers.LogError("thumbnail", err)
-	}
-
-	err = os.RemoveAll(thumbnailcachedir)
+	err = os.Chtimes(ai.thumbnailfilepath, time.Now().Local(), ai.ModTime())
 	helpers.LogError("thumbnail", err)
 
 	// In Xfce, the new thumbnail is not shown in the file manager until we touch the file
@@ -195,41 +173,20 @@ genericIcon:
 }
 
 // Convert a given file into a PNG; its dependencies add about 2 MB to the executable
-func convertToPng(filePath string) error {
-	// Strange colors: https://github.com/srwiley/oksvg/issues/15
-	icon, err := ReadIcon(filePath, WarnErrorMode)
+func convertToPng(reader io.Reader) (*bytes.Buffer, error) {
+	icon, err := oksvg.ReadIconStream(reader, oksvg.WarnErrorMode)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	w, h := int(icon.ViewBox.W), int(icon.ViewBox.H)
 	img := image.NewRGBA(image.Rect(0, 0, w, h))
-	scannerGV := NewScannerGV(w, h, img, img.Bounds())
-	raster := NewDasher(w, h, scannerGV)
+	scannerGV := rasterx.NewScannerGV(w, h, img, img.Bounds())
+	raster := rasterx.NewDasher(w, h, scannerGV)
 	icon.Draw(raster, 1.0)
-	err = saveToPngFile(filePath, img)
+	var out bytes.Buffer
+	err = png.Encode(&out, img)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
-}
-
-func saveToPngFile(filePath string, m image.Image) error {
-	// Create the file
-	f, err := os.Create(filePath)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	// Create Writer from file
-	b := bufio.NewWriter(f)
-	// Write the image into the buffer
-	err = png.Encode(b, m)
-	if err != nil {
-		return err
-	}
-	err = b.Flush()
-	if err != nil {
-		return err
-	}
-	return nil
+	return &out, nil
 }
